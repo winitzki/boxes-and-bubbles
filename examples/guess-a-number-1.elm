42e3058f1a2eb3b95d794e8adce175d2add4c085
Sergei Winitzki <winitzki@gmail.com>
2017-05-24 21:16:08 -0700

    implement sessions and async answers

--- examples/guess-a-number-1.elm
+++ examples/guess-a-number-1.elm
@@ -15,6 +15,7 @@ import Element exposing (..)
 import Color exposing (..)
 import Text exposing (fromString)
 import String
+import Array exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
@@ -25,7 +26,7 @@ import Task exposing (perform)
 
 type Msg = Reset
  | Request
- | Answer
+ | Answer (String, Int)
  | Ask String
  | SetSecret Int
 
@@ -35,33 +36,38 @@ type alias Model = {
     n: Int
     , tries: Int
     , current: String
-    , message: String
+    , messages: Array String
+    , session: Int
  }
 
-initModel = { n = 20, tries = 0, current = "", message = "" }
+initModel : Model
+initModel = { n = 20, tries = 0, current = "", messages = Array.empty, session = 0 }
 
 initFill = Random.generate SetSecret (Random.int 1 upperLimit)
 
 updateAll: Msg -> Model -> (Model, Cmd Msg)
 updateAll msg model =
   case msg of
-    Reset -> (initModel, initFill)
+    Reset -> ({ initModel | session = model.session + 1 }, initFill)
     SetSecret x -> ( { model | n = x }, Cmd.none)
     Ask x -> ( { model | current = x }, Cmd.none)
-    Request -> (model, perform (\_ -> Answer) (sleep (10 * second)) )
-    Answer ->
-      let
-        newMessage = case String.toInt model.current of
-           Err msg -> "Error: bad numeric format"
-           Ok x ->
-             if model.n == x then "you got it!" else
-             if model.n > x then
-              "it is greater than " ++ toString x
-             else
-              "it is not greater than " ++ toString x
-        newModel = { model | tries = model.tries + 1, message = newMessage }
-      in
-        (newModel, Cmd.none)
+    Request -> (model, perform (\_ -> Answer (model.current, model.session)) (sleep (10 * second)) )
+    Answer (current, session) ->
+      if session == model.session then
+          let
+            newMessage = case String.toInt current of
+               Err msg -> "Error: bad numeric format"
+               Ok x ->
+                 if model.n == x then "you got it, the answer is " ++ toString x
+                 else if model.n > x then
+                  "it is greater than " ++ toString x
+                 else
+                  "it is not greater than " ++ toString x
+            newModel = { model | tries = model.tries + 1, messages = push newMessage model.messages }
+          in
+            (newModel, Cmd.none)
+
+      else (model, Cmd.none)
 
 --    _ -> (model, Cmd.none)
 
@@ -76,9 +82,9 @@ scene model =
     , input [ placeholder "your guess", onInput Ask, myStyle ] []
     , text " "
     , button [ onClick Request ] [ text "?" ]
-    , div [ myStyle2 ] [ text model.message ]
     , div [] []
     , text ("You made " ++ toString model.tries ++ " tries")
+    , div [ myStyle2 ] (Array.toList (Array.map (\m -> p [] [text m]) model.messages))
     ]
 
 myStyle =
